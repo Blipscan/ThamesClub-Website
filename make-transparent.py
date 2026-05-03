6a93a4864628d44031a8bb730dc463484d659e27
Blipscan <thamesentertainment@gmail.com>
2026-05-03 15:49:38 -0400

    Deploy Thames Club site

---
+++ make-transparent.py
@@ -0,0 +1,22 @@
+﻿from PIL import Image
+import os
+
+src = r"C:\Users\super\Documents\Projects\ThamesClubSite\deploy\images\above-below_01.jpg"
+out = r"C:\Users\super\Documents\Projects\ThamesClubSite\deploy\images\above-below_01.png"
+
+print("SRC EXISTS:", os.path.exists(src))
+
+img = Image.open(src).convert("RGBA")
+pixels = []
+
+for r, g, b, a in img.getdata():
+    if r > 220 and g > 220 and b > 220:
+        pixels.append((255, 255, 255, 0))
+    else:
+        pixels.append((255, 255, 255, 255))
+
+img.putdata(pixels)
+img.save(out)
+
+print("OUT EXISTS:", os.path.exists(out))
+print(out)
